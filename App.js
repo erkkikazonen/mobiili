42090191581b8827ee04bc15e75d1429fdfa9d05
Erkki Kazonen <erkki.kazonen@gmail.com>
2024-01-19 10:53:45 +0200

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleSum = () => {
+    const sum = parseFloat(number1) + parseFloat(number2);
+    setResult("Result: " + sum);
+  };
+
+  const handleDif = () => {
+  const dif = parseFloat(number1) - parseFloat(number2);
+  setResult("Result: " + dif);
+};
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{result}</Text>
+      <TextInput
+        style={{ width: 200, borderColor: "gray", borderWidth: 1 }}
+        keyboardType="numeric"
+        onChangeText={(text) => setNumber1(text)}
+        value={number1}
+      />
+      <TextInput
+        style={{ width: 200, borderColor: "gray", borderWidth: 1 }}
+        keyboardType="numeric"
+        onChangeText={(text) => setNumber2(text)}
+        value={number2}
+      />
+      <View
+        style={styles.Button}>
+      <Button title="+" onPress={handleSum} />
+      <Button title="-" onPress={handleDif} />
+        </View>
+        <StatusBar style="auto" />
     </View>
   );
 }
@@ -13,8 +46,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  input: {
+    width: 200,
+    borderColor: "gray",
+    borderWidth: 1,
+  },
+  Button: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "30%",
+    marginTop: 20,
+  }
 });
