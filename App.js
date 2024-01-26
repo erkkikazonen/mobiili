a8f66a67f51ee2e6433d8b337ae2b9e50992bc33
Erkki Kazonen <erkki.kazonen@gmail.com>
2024-01-26 14:01:35 +0200

    history

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View, FlatList } from "react-native";
 
 export default function App() {
   const [number1, setNumber1] = useState("");
   const [number2, setNumber2] = useState("");
   const [result, setResult] = useState("");
+  const [maths, setMaths] = useState([]);
 
   const handleSum = () => {
     const sum = parseFloat(number1) + parseFloat(number2);
     setResult("Result: " + sum);
+    handleAdd(`${number1} + ${number2} = ${sum}`)
   };
 
   const handleDif = () => {
   const dif = parseFloat(number1) - parseFloat(number2);
-  setResult("Result: " + dif);
-};
+    setResult("Result: " + dif);
+    handleAdd(`${number1} - ${number2} = ${dif}`)
+  };
+  
+  const handleAdd = (history) => {
+    setMaths([...maths, history])
+  }
 
 
   return (
@@ -37,7 +44,17 @@ export default function App() {
         style={styles.Button}>
       <Button title="+" onPress={handleSum} />
       <Button title="-" onPress={handleDif} />
-        </View>
+      </View>
+      <View>
+        <Text style={{ flexDirection: 'row', alignItems: "center" }}>History</Text>
+        <FlatList
+          data={maths}
+          renderItem={({ item }) =>
+            <View style={styles.listItem}>
+              <Text style={{ fontSize: 18 }}>{item}</Text>
+            </View>}
+        />
+      </View>
         <StatusBar style="auto" />
     </View>
   );
@@ -49,6 +66,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    marginTop: 200,
   },
   input: {
     width: 200,
@@ -58,7 +76,15 @@ const styles = StyleSheet.create({
   Button: {
     flexDirection: "row",
     justifyContent: "space-around",
-    width: "30%",
-    marginTop: 20,
+    width: "10%",
+    margin: 20,
+  },
+  listItem: {
+    flex: 2,
+    flexDirection: 'row',
+    alignItems: "center",
+    justifyContent: "center",
+    width: 300,
+    padding: 10
   }
 });
